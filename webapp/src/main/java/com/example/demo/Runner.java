38cd57e9a6ccbc8e042113f7438a6ed173ccad33
study <study@LAPTOP-BP8LHK5S>
2020-05-18 23:33:47 +0900

    springboot_rabbitmq test

---
+++ webapp/src/main/java/com/example/demo/Runner.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Runner implements CommandLineRunner {
+
+	private final RabbitTemplate rabbitTemplate;
+	private final Receiver receiver;
+	
+	static int sendCount = 0;
+
+	public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
+		this.receiver = receiver;
+		this.rabbitTemplate = rabbitTemplate;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Sending message...");
+		rabbitTemplate.convertAndSend(WebappApplication.topicExchangeName, 
+				"foo.bar.baz", "Hello from RabbitMQ!--" + ++sendCount);
+//		receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+		receiver.getLatch().await(3 * 1000, TimeUnit.MILLISECONDS);
+	}
+
+}
